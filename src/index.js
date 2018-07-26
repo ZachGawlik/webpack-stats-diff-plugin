58cbef8e4d5fdff717d9c37706cdb083f4e4c670
Zach Gawlik <ZachGawlik@users.noreply.github.com>
2018-07-25 22:26:38 -0400

    feat: Enable comparing to previous build

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+/* eslint-disable no-console */
 const path = require('path');
 const fs = require('fs');
-const { getStatsDiff, printStatsDiff } = require('webpack-stats-diff');
+const {
+  getAssetsDiff,
+  getStatsDiff,
+  printStatsDiff
+} = require('webpack-stats-diff');
+const {
+  measureFileSizesBeforeBuild
+} = require('react-dev-utils/FileSizeReporter');
 
 class StatsDiffPlugin {
   constructor(opts) {
-    if (!opts.oldStatsFile) {
-      throw new Error('oldStatsFile not specified in StatsDiffPlugin config');
-    }
     this.oldStatsFile = opts.oldStatsFile;
     this.config = {
       extensions: opts.extensions,
@@ -16,24 +21,56 @@ class StatsDiffPlugin {
 
   apply(compiler) {
     if (compiler.hooks) {
-      compiler.hooks.emit.tapAsync(
+      if (!this.oldStatsFile) {
+        compiler.hooks.beforeRun.tapAsync(
+          'calculate-previous-size',
+          this.getPreviousBuildSize.bind(this)
+        );
+      }
+      compiler.hooks.afterEmit.tapAsync(
         'webpack-stats-diff-plugin',
         this.getStatsDiff.bind(this)
       );
     } else {
-      compiler.plugin('emit', this.getStatsDiff.bind(this));
+      if (!this.oldStatsFile) {
+        compiler.plugin('beforeRun', this.getPreviousBuildSize.bind(this));
+      }
+      compiler.plugin('afterEmit', this.getStatsDiff.bind(this));
     }
   }
 
+  getPreviousBuildSize(compiler, callback) {
+    this.buildRoot = compiler.options.output.path;
+    measureFileSizesBeforeBuild(this.buildRoot).then(({ sizes }) => {
+      this.sizesBeforeBuild = sizes;
+      callback();
+    });
+  }
+
   getStatsDiff(compilation, callback) {
-    const { assets } = compilation.getStats().toJson();
-    const oldPath = path.resolve(process.cwd(), this.oldStatsFile);
-    if (!fs.existsSync(oldPath)) {
-      return callback(new Error('File does not exist'));
+    if (this.oldStatsFile) {
+      const oldPath = path.resolve(process.cwd(), this.oldStatsFile);
+      if (!fs.existsSync(oldPath)) {
+        return callback(new Error('File does not exist'));
+      }
+      const oldAssets = require(oldPath).assets;
+      const { assets } = compilation.getStats().toJson();
+      console.log(`Comparing build sizes to ${this.oldStatsFile}`);
+      printStatsDiff(getStatsDiff(oldAssets, assets, this.config), null, 2);
+      callback();
+    } else {
+      measureFileSizesBeforeBuild(this.buildRoot).then(({ sizes }) => {
+        console.log(
+          `Comparing build sizes to prior contents of ${this.buildRoot}`
+        );
+        printStatsDiff(
+          getAssetsDiff(this.sizesBeforeBuild, sizes, this.config),
+          null,
+          2
+        );
+        callback();
+      });
     }
-    const oldAssets = require(oldPath).assets;
-    printStatsDiff(getStatsDiff(oldAssets, assets, this.config), null, 2);
-    callback();
   }
 }
 
