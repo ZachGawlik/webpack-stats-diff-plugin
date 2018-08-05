6b549d798ac383bc445cdfa42048f3fbae176b57
Zach Gawlik <ZachGawlik@users.noreply.github.com>
2018-08-05 13:22:29 -0400

    chore(examples): Simplify example webpack confrigs

--- examples/webpack-2/webpack.config.js
+++ examples/webpack-2/webpack.config.js
@@ -8,58 +8,56 @@ const PATHS = {
   build: path.resolve(__dirname, 'dist')
 };
 
-module.exports = () => {
-  return {
-    entry: {
-      entryA: './src/Main.js',
-      entryB: './src/About.js',
-      entryC: './src/EntryC.js'
-    },
-    resolve: {
-      modules: ['node_modules', path.resolve(__dirname, '../../node_modules')]
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-        {
-          test: /\.(jpg|png)$/,
-          use: {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[hash].[ext]'
-            }
+module.exports = () => ({
+  entry: {
+    entryA: './src/Main.js',
+    entryB: './src/About.js',
+    entryC: './src/EntryC.js'
+  },
+  resolve: {
+    modules: ['node_modules', path.resolve(__dirname, '../../node_modules')]
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]'
           }
-        },
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: 'css-loader'
-          })
         }
-      ]
-    },
-    output: {
-      filename: '[name].[chunkhash].js',
-      chunkFilename: '[name].[chunkhash].chunk.js',
-      path: PATHS.build
-    },
-    stats: {
-      chunks: false,
-      maxModules: 0
-    },
-    plugins: [
-      new CleanWebpackPlugin([PATHS.build], { beforeEmit: true }),
-      new ExtractTextPlugin('[name].[contenthash].css'),
-      process.env.COMMONS_CHUNKS === 'true' &&
-        new webpack.optimize.CommonsChunkPlugin({
-          name: 'commons',
-          filename: 'commons.js'
-        }),
-      (process.env.COMPARE_PREVIOUS === 'true' || process.env.STATS_FILE) &&
-        new WebpackStatsDiffPlugin({
-          oldStatsFile: process.env.STATS_FILE
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
         })
-    ].filter(Boolean)
-  };
-};
+      }
+    ]
+  },
+  output: {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].chunk.js',
+    path: PATHS.build
+  },
+  stats: {
+    chunks: false,
+    maxModules: 0
+  },
+  plugins: [
+    new CleanWebpackPlugin([PATHS.build], { beforeEmit: true }),
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    process.env.COMMONS_CHUNKS === 'true' &&
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'commons',
+        filename: 'commons.js'
+      }),
+    (process.env.COMPARE_PREVIOUS === 'true' || process.env.STATS_FILE) &&
+      new WebpackStatsDiffPlugin({
+        oldStatsFile: process.env.STATS_FILE
+      })
+  ].filter(Boolean)
+});
--- examples/webpack-4/webpack.config.js
+++ examples/webpack-4/webpack.config.js
@@ -7,56 +7,54 @@ const PATHS = {
   build: path.resolve(__dirname, 'dist')
 };
 
-module.exports = (env = {}) => {
-  return {
-    mode: 'production',
-    entry: {
-      entryA: './src/Main.js',
-      entryB: './src/About.js',
-      entryC: './src/EntryC.js'
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-        {
-          test: /\.(jpg|png)$/,
-          use: {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[hash].[ext]'
-            }
+module.exports = () => ({
+  mode: 'production',
+  entry: {
+    entryA: './src/Main.js',
+    entryB: './src/About.js',
+    entryC: './src/EntryC.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]'
           }
-        },
-        {
-          test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader']
         }
-      ]
-    },
-    output: {
-      filename: '[name].[chunkhash].js',
-      chunkFilename: '[name].[chunkhash].chunk.js',
-      path: PATHS.build
-    },
-    stats: {
-      chunks: false,
-      maxModules: 0
-    },
-    plugins: [
-      new CleanWebpackPlugin([PATHS.build]),
-      new MiniCssExtractPlugin({
-        filename: '[name].[chunkhash].css',
-        chunkFilename: '[name].[chunkhash].css'
-      }),
-      (process.env.COMPARE_PREVIOUS === 'true' || process.env.STATS_FILE) &&
-        new WebpackStatsDiffPlugin({
-          oldStatsFile: process.env.STATS_FILE
-        })
-    ].filter(Boolean),
-    optimization: {
-      splitChunks: {
-        chunks: process.env.CHUNKS_ALL === 'true' ? 'all' : 'async'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
+    ]
+  },
+  output: {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].chunk.js',
+    path: PATHS.build
+  },
+  stats: {
+    chunks: false,
+    maxModules: 0
+  },
+  plugins: [
+    new CleanWebpackPlugin([PATHS.build], { beforeEmit: true }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[chunkhash].css',
+      chunkFilename: '[name].[chunkhash].css'
+    }),
+    (process.env.COMPARE_PREVIOUS === 'true' || process.env.STATS_FILE) &&
+      new WebpackStatsDiffPlugin({
+        oldStatsFile: process.env.STATS_FILE
+      })
+  ].filter(Boolean),
+  optimization: {
+    splitChunks: {
+      chunks: process.env.CHUNKS_ALL === 'true' ? 'all' : 'async'
     }
-  };
-};
+  }
+});
